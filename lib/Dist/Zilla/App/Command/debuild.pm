68ca25f93039b5514c84e45409151a7d68400624
Vyacheslav Matjukhin <mmcleric@yandex-team.ru>
2010-11-11 23:17:51 +0300

    debuild command propagates some options

--- lib/Dist/Zilla/App/Command/debuild.pm
+++ lib/Dist/Zilla/App/Command/debuild.pm
@@ -18,7 +18,17 @@ use autodie qw(:all);
 
 sub abstract { 'build debian package' }
 
-sub opt_spec {}
+sub opt_spec {
+    # these options are propagated to debuild mostly for the tests
+    # note than they should be specified as --us and will be transformed to -us because of getopt parsing differences
+    ['us'   => "do not sign the source package"],
+    ['uc'   => "do not sign the .changes file"],
+}
+
+sub validate_args {
+    my ($self, $opt, $args) = @_;
+    die 'no args expected' if @$args;
+}
 
 sub execute {
     my ($self, $opt, $args) = @_;
@@ -26,7 +36,10 @@ sub execute {
     system('rm -rf debuild');
     mkdir('debuild');
     $self->zilla->build_in('debuild/source');
-    system('cd debuild/source && debuild');
+    my @debuild_args;
+    push @debuild_args, '-us' if $opt->{us};
+    push @debuild_args, '-uc' if $opt->{uc};
+    system("cd debuild/source && debuild @debuild_args");
 }
 
 1;
