2a01625fa7e5a1dedab1d0406c0ce707e99221fd
Knut Arne Bjørndal <knut.arne.bjorndal@easyconnect.no>
2013-01-04 13:33:45 +0100

    Fix a few small bugs, closes #2

--- lib/Dist/Zilla/Stash/Deb.pm
+++ lib/Dist/Zilla/Stash/Deb.pm
@@ -2,6 +2,8 @@ package Dist::Zilla::Stash::Deb;
 use Moose;
 with 'Dist::Zilla::Role::Stash';
 
+# ABSTRACT: Stash for Dist::Zilla::Deb variables
+
 has desc => (is => 'ro', isa => 'ArrayRef[Str]');
 has architecture => (is => 'ro', isa => 'Str');
 sub mvp_multivalue_args { return qw(desc) }
